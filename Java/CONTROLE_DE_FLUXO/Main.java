3ffab05f0865416d7658770ac54ae18a68bd4f6f
João Baptista <joaovictorb.profissional@gmail.com>
2022-10-15 09:26:19 -0300

    adicionei exemplos de estruturar de controle de fluxo(if/else) e repetição

---
+++ Java/CONTROLE_DE_FLUXO/Main.java
@@ -0,0 +1,14 @@
+package CONTROLE_DE_FLUXO;
+
+public class Main {
+    public static void main(String[] args){
+
+        /*Para o vontrole de fluxo pode-se usar a estrutura de if / else e case
+         * Exemplo de ternario: String idade = var >= 18 ? "adulto" : var < 18 && var >= 13 ? "adolecente" : "criança"
+         * 
+         * 
+         * 
+         */
+        
+    }
+}
---
+++ Java/REPETICAO/Main.java
@@ -0,0 +1,74 @@
+package REPETICAO;
+import java.util.Scanner;
+
+public class Main {
+    public static void breakLine(){
+        for(int i = 1; i <= 3; i++){
+            switch(i){
+                case 1:
+                case 3:
+                System.out.println("\n");
+                continue;
+            }
+            System.out.println("-------------------------------------------------------------");
+        }
+    }
+    
+    public static void main(String[] args){
+
+        Scanner leitor = new Scanner(System.in);
+
+        // Estrutura de repetição for
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);;
+        }
+        
+        breakLine();
+
+        // Estrutura de repetição while
+        int x = 1;
+        while(x++ < 10){
+            System.out.println(x);
+        
+        }
+
+        breakLine();
+
+        // FOR EM ARRAY
+        String jogadores [] = {"João" , "Marcos", "Pedro", "Isabela", "Leticia"};
+
+        for(int cont = 0; cont < jogadores.length; cont++){
+            System.out.println("O jogador no indice " + cont + " = " + jogadores[cont] + "\n");
+        }
+
+        breakLine();
+
+        // FOR EM ARRAY (diferente
+        // a cada execução, a variavel time ira receber um novo valor de acordo com o array jogadores
+        for(String nome : jogadores){
+            System.out.println("nome do jogador: " + nome);
+        }
+
+        breakLine();
+
+        //Laço Do while
+        
+        int verificador;
+
+       System.out.println("digite 'Sim' para continuar: ");
+       verificador = leitor.nextInt();
+
+        do{ 
+            
+            if(verificador != 1){
+            System.out.println("Você digitou uma opsão invalida!\nDigite novamente: ");
+            verificador = leitor.nextInt();
+            }
+
+        }while(verificador != 1); 
+
+        leitor.close();
+        
+    }
+    
+}
